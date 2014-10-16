7640df70b2eee4ff72cb7ccc4d2f05be5ddb6e0f
yuki3738 <y.minamiya@gmail.com>
2014-10-17 01:24:36 +0900

    Omit :each param 次はbe_[predicate] マッチャーから

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,12 +1,14 @@
 require_relative 'message_filter'
 
 describe MessageFilter do
+  before do
+    @filter = MessageFilter.new('foo')
+  end
+
   it 'detects message with NG word' do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello from foo')).to eq true
+    expect(@filter.detect?('hello from foo')).to eq true
   end
   it 'does not detect message without NG word' do
-    filter = MessageFilter.new('foo')
-    expect(filter.detect?('hello, world!')).to eq false
+    expect(@filter.detect?('hello, world!')).to eq false
   end
 end
